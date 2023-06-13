7edf826e9d1904ce95a7fba6066f41cb887e5bf2
Zijian ZHONG <593958169@qq.com>
2023-06-14 00:08:01 +0800

    rewritten to improve naming and efficiency

--- src/Core.py
+++ src/Core.py
@@ -6,31 +6,31 @@ def pretreatment(ima : Image, threshold : int):
     # Generate binary image by setting a threshold
     ima = ima.convert('L') # Convert to grayscale picture
     im = numpy.array(ima) # Convert to numpy array
-    for i in range(im.shape[0]):  # Convert to 2D
-        for j in range(im.shape[1]):
-            if im[i, j] > threshold:
-                im[i, j] = 0
-            else:
-                im[i, j] = 1
+    im = numpy.where(im > threshold, 0, 1)
     return im
 
+def processImage(ima : Image, threshold : int = 128):
+    # Print the image in the form of bytes
+    im = pretreatment(ima, threshold)
+    out = []
+    for row in im:
+        # Traverse each row in the image
+        count = 0
+        count_1 = ''
+        for pix in row:
+            count += 1
+            count_1 = count_1 + str(pix)
+            if count % 8 == 0:
+                out.append(int(count_1, 2))
+                count_1=''
+    cnt = 0
+    for num in out:
+        cnt += 1
+        print('0x%02X' % num, end=",")
+        if cnt % 16 == 0:
+            print(" ")
 
-ima = Image.open('BPmeter.png') # Read the image
-im = pretreatment(ima)
-out = []
-for i in im:
-    count = 0
-    count_1 = ''
-    for j in i:
-        count += 1
-        count_1 = count_1+str(j)
-        if count % 8 ==0:
-            out.append(int(count_1,2))
-            count_1=''
-cnt = 0
-for i in out:
-    cnt += 1
-    print('0x%02X'%i,end=",")
-    if cnt % 16 == 0:
-        print(" ")
-
+if __name__ == '__main__':
+    image = Image.open("res/test.png")
+    processImage(image, 128)
+    
